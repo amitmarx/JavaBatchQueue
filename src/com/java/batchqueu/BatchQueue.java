9b7693c92b1b24d078672c44c5d42202b5b5d9f6
Amit Marx <amitm@nonamesecurity.com>
2021-06-07 02:51:34 +0300

    Batch queue consider size and time limits

---
+++ src/com/java/batchqueu/BatchQueue.java
@@ -0,0 +1,53 @@
+package com.java.batchqueu;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class BatchQueue<T> implements AutoCloseable {
+    private final ArrayBlockingQueue<T> queue;
+    private final Consumer<ArrayList<T>> callback;
+    private final AtomicBoolean isCleanInProcess = new AtomicBoolean(false);
+    private final ScheduledExecutorService scheduleExecutor = Executors.newSingleThreadScheduledExecutor();
+    private final ExecutorService callbackExecution = Executors.newSingleThreadExecutor();
+
+
+    public BatchQueue(int sizeLimit, long timeLimit, Consumer<ArrayList<T>> callback) {
+        this.queue = new ArrayBlockingQueue<>(sizeLimit);
+        this.callback = callback;
+        this.scheduleExecutor.scheduleWithFixedDelay(
+                () -> this.cleanQueueIfNeeded(true),
+                timeLimit,
+                timeLimit,
+                TimeUnit.MILLISECONDS);
+    }
+
+    public void push(T item) {
+        boolean isAdded = queue.offer(item);
+        if (!isAdded) {
+            cleanQueueIfNeeded(false);
+            this.push(item);
+        }
+    }
+
+    private void cleanQueueIfNeeded(boolean ignoreCapacity) {
+        boolean isFirstCleaning = this.isCleanInProcess.compareAndSet(false, true);
+        if (isFirstCleaning) {
+            if (ignoreCapacity || this.queue.remainingCapacity() == 0) {
+                ArrayList<T> items = new ArrayList<>(this.queue.size());
+                int numberOfItems = this.queue.drainTo(items);
+                if (numberOfItems > 0) {
+                    callbackExecution.submit(() -> this.callback.accept(items));
+                }
+            }
+            this.isCleanInProcess.set(false);
+        }
+    }
+
+    @Override
+    public void close() {
+        this.scheduleExecutor.shutdown();
+        this.callbackExecution.shutdown();
+    }
+}
---
+++ src/com/java/batchqueu/Test.java
@@ -0,0 +1,26 @@
+package com.java.batchqueu;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (
+                BatchQueue<String> batch = new BatchQueue<>(4, 100, items -> {
+                    System.out.println(Arrays.toString(items.toArray()));
+                    System.out.println("=================");
+                })
+        ) {
+
+            batch.push("1");
+            batch.push("2");
+            Thread.sleep(90);
+            batch.push("3");
+            batch.push("4");
+            batch.push("5");
+            batch.push("6");
+            batch.push("7");
+            Thread.sleep(50);
+        }
+    }
+}
