a02c50578679a4538c4303172d2c739d4b51539a
Amit Marx <amitm@nonamesecurity.com>
2021-06-07 10:03:00 +0300

    Dummy PR

--- src/com/java/batchqueu/Test.java
+++ src/com/java/batchqueu/Test.java
@@ -20,6 +20,8 @@ public class Test {
             batch.push("5");
             batch.push("6");
             batch.push("7");
+            batch.push("8");
+            batch.push("9");
             Thread.sleep(500);
         }
     }
